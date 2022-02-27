4063f0de2edfef0a42cbeeb896dd98547ae9fa51
CS436finalProject <1652765684@qq.com>
2022-02-27 18:00:38 -0500

    update

---
+++ src/components/sections/Articles.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Article from "../elements/Article";
+
+class Articles extends Component {
+  constructor(props) {
+    super();
+    this.state = { articles: [] };
+  }
+
+  componentDidMount() {
+    const devTo = "https://dev.to/api/articles?username=jcoelho";
+
+    fetch(devTo)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let articles = [];
+        data = data.slice(0, 4);
+        data.forEach((element, index) => {
+          articles.push(
+            <div className="column" key={index}>
+              <Article
+                key={index}
+                title={element.title}
+                url={element.url}
+                image={element.cover_image}
+                extract={element.description}
+              />
+            </div>
+          );
+        });
+        var offset = 4 - data.length;
+        for (var i = 0; i < offset; i++) {
+          articles.push(<div className="column"></div>);
+        }
+        this.setState({ articles: articles });
+      });
+  }
+
+  render() {
+    return (
+      <section className="section" id="articles">
+      </section>
+    );
+  }
+}
+
+export default Articles;
